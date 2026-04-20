1835c485146acb2db53a585ab6e2193caa09b9e1
yasmine <lajdelyasmine12345@gmail.com>
2026-04-20 17:06:18 +0100

    Fix CORS pour Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,15 @@
-// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    outDir: 'dist',
-  },
+        changeOrigin: true
+      }
+    }
+  }
 });
