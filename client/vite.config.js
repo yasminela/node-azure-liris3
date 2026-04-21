85a7025d4b1f5e865ca5d62b42ea70de22bb3e3a
yasmine <lajdelyasmine12345@gmail.com>
2026-04-21 12:39:42 +0100

    Fix routing Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,24 @@
-// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
-    host: '0.0.0.0',
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+        changeOrigin: true
+      }
+    }
   },
   build: {
     outDir: 'dist',
-  },
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 });
