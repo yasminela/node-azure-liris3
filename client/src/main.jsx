c108d3dcb6d1d2518619d18d8172cb50161ebf35
yasmine <lajdelyasmine12345@gmail.com>
2026-04-21 12:58:14 +0100

    Fix BrowserRouter import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,12 @@ library.add(fas, far);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <App />
     </BrowserRouter>
   </React.StrictMode>
